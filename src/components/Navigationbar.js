75215429082dc59f4d5db38f08eef540127272f1
Arun3sh <m.aruneshwar@gmail.com>
2022-08-09 22:54:18 +0530

    Navbar added

--- src/components/Navigationbar.js
+++ src/components/Navigationbar.js
@@ -1,7 +1,22 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 function Navigationbar() {
   return (
     <div className="container-sm nav-container">
-      <div className="container-sm nav-wrapper">Navigation Bar</div>
+      <div className="container-sm nav-wrapper">
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Fitness🦾Tracker</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Calorie Calculator</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
     </div>
   );
 }
