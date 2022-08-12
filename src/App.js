a642458b6a50fa7dd8bfe4fef930b9de317fbae9
Arun3sh <m.aruneshwar@gmail.com>
2022-08-12 22:48:57 +0530

    roue initiated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
 import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { Switch, Route } from "react-router-dom";
+import Navigationbar from "./components/Navigationbar";
+import Home from "./components/pages/Home";
 
 function App() {
-  return <div className="App">Home</div>;
+  return (
+    <div className="App">
+      <Navigationbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
